54b9019286ca767055cc12d26481834cbc9a828a
VishalTuran <vishalturan192@gmail.com>
2021-04-13 10:25:01 +0530

    Integrated Docker

--- src/main/java/helper/Helper.java
+++ src/main/java/helper/Helper.java
@@ -2,23 +2,32 @@ package helper;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Helper {
-    public WebDriver driver;
+//    public WebDriver driver;
+    public static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
+    public static String remote_url = "http://192.168.99.100:4546/wd/hub";
 
-    public void setUpDriver(String browserName){
+    public void setUpDriver(String browserName) throws MalformedURLException {
         int driverVersion=89;
         String dir=System.getProperty("user.dir");
         String pathToDriver=dir+"\\src\\main\\java\\helper\\chromedriver_"+driverVersion+".exe";
         System.setProperty("webdriver.chrome.driver",pathToDriver);
+        ChromeOptions options = new ChromeOptions();
         if(browserName.equalsIgnoreCase("chrome")){
-            driver=new ChromeDriver();
+//            driver=new ChromeDriver();
+            driver.set(new RemoteWebDriver(new URL(remote_url), options));
         }
     }
 
     public void tearDown(){
         if(driver!=null)
-            driver.quit();
+            driver.get().quit();
         else
             return;
     }
--- src/test/java/SampleTestCase.java
+++ src/test/java/SampleTestCase.java
@@ -1,16 +1,19 @@
 import helper.Helper;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
 
 public class SampleTestCase {
     Helper helper;
     WebDriver driver;
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() throws MalformedURLException {
         helper=new Helper();
         helper.setUpDriver("chrome");
     }
@@ -18,8 +21,13 @@ public class SampleTestCase {
     @Test
     public void assertTitle(){
         String expectedTitle="Google";
-        driver=helper.driver;
+        driver=helper.driver.get();
         driver.get("https://www.google.com");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         String actualTitle=driver.getTitle();
         Assert.assertEquals(expectedTitle,actualTitle,"Title Not Matching");
 }
